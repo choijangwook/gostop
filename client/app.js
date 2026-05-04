874251d8544fc232329b904d0d6e41db879b33b1
choi jang wook <jangwookchoi1@gmail.com>
2026-05-05 00:41:16 +0900

    Implement socket.io functionality for game rooms

--- client/app.js
+++ client/app.js
@@ -1,1 +1,31 @@
+const socket = io();
 
+function createRoom() {
+  socket.emit("createRoom");
+}
+
+function joinRoom() {
+  const roomId = document.getElementById("roomInput").value;
+  socket.emit("joinRoom", roomId);
+}
+
+socket.on("roomCreated", (roomId) => {
+  alert("방 코드: " + roomId);
+});
+
+socket.on("startGame", (state) => {
+  console.log(state);
+  render(state);
+});
+
+function render(state) {
+  const game = document.getElementById("game");
+
+  game.innerHTML = `
+    <h3>내 카드 (${state.player.length})</h3>
+    <pre>${JSON.stringify(state.player, null, 2)}</pre>
+
+    <h3>바닥 (${state.table.length})</h3>
+    <pre>${JSON.stringify(state.table, null, 2)}</pre>
+  `;
+}
