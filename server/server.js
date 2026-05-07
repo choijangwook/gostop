ab46aa684dac07071b263328e93862a9e9f3bcd2
choi jang wook <jangwookchoi1@gmail.com>
2026-05-07 15:47:18 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,42 +2,46 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
-const game = require("./game");
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static("docs"));
 
+const rooms = {};
+
+// =========================
+// socket 연결
+// =========================
 io.on("connection", (socket) => {
+
   console.log("접속:", socket.id);
 
-  socket.on("createRoom", ({ roomId }) => {
-    game.createRoom(roomId);
-    socket.join(roomId);
-  });
-
+  // =========================
+  // 방 참가
+  // =========================
   socket.on("joinRoom", ({ roomId }) => {
+
+    roomId = Number(roomId);
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = {
+        players: []
+      };
+    }
+
     socket.join(roomId);
-    game.joinRoom(roomId, socket.id);
 
-    io.to(roomId).emit("stateUpdate", game.getState(roomId));
+    rooms[roomId].players.push(socket.id);
+
+    io.to(roomId).emit("stateUpdate", rooms[roomId]);
   });
 
-  socket.on("takeCard", ({ roomId, cardId }) => {
-    game.takeCard(roomId, socket.id, cardId);
-
-    io.to(roomId).emit("stateUpdate", game.getState(roomId));
-  });
-
-  socket.on("endTurn", ({ roomId }) => {
-    game.endTurn(roomId);
-
-    io.to(roomId).emit("stateUpdate", game.getState(roomId));
-  });
 });
 
+// =========================
+// 서버 실행
+// =========================
 server.listen(10000, () => {
-  console.log("🎴 Gostop 서버 실행중");
+  console.log("🎴 GoStop server running");
 });
